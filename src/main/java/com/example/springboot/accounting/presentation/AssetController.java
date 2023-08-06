9844143f95cef4bfc4cf5408c54b402f00aa7f7e
b62303411 <samuel.ars@gmail.com>
2023-08-06 08:25:01 -0400

    asset are now showed correctly

--- src/main/java/com/example/springboot/accounting/presentation/AssetController.java
+++ src/main/java/com/example/springboot/accounting/presentation/AssetController.java
@@ -1,6 +1,8 @@
 package com.example.springboot.accounting.presentation;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,46 +23,51 @@ import com.example.springboot.accounting.service.TransactionService;
 public class AssetController {
 	private AssetService assetService;
 	private TransactionService transactionService;
+
 	@Autowired
-	public AssetController(AssetService assetService,TransactionService transactionService) 
-	{
-		this.assetService=assetService;
+	public AssetController(AssetService assetService, TransactionService transactionService) {
+		this.assetService = assetService;
 		this.transactionService = transactionService;
 	}
-	
+
 	@GetMapping
-	public String assets(Model model) 
-	{	
+	public String assets(Model model) {
 		List<Asset> assets = assetService.findAll();
 		model.addAttribute("assets", assets);
 		model.addAttribute("year", 2023);
 		return "assets";
 	}
-	
+
 	@GetMapping("/create/{transactionId}")
-	public String transactions(Model model, @PathVariable("transactionId") Long transactionId) 
-	{	
-		AssetCreationRequest request= new AssetCreationRequest();
-		request.transactionId=transactionId;
+	public String transactions(Model model, @PathVariable("transactionId") Long transactionId) {
+		AssetCreationRequest request = new AssetCreationRequest();
+		request.transactionId = transactionId;
 		Transaction transaction = transactionService.findById(transactionId);
 		model.addAttribute("transaction", transaction);
 		model.addAttribute("request", request);
 		model.addAttribute("year", 2023);
-	
+
 		return "assetCreation";
 	}
-	
-	@GetMapping("/execute-leg/{transactionId}")
-	public void executeLeg(Model model, @PathVariable("legId") Long legId)
-	{
+
+	@GetMapping("/execute-leg/{legId}")
+	public String executeLeg(Model model, @PathVariable("legId") Long legId) {
 		AmortisationLeg leg = assetService.findLeg(legId);
-		Asset asset = leg.getAmortisation().getAsset();
-		Double currentValue = asset.getCurrentValue();
-		currentValue = leg.getAmount(); // Amount of the depreciation.
-		asset.setCurrentValue(currentValue );
-		leg.setRealized(true);
-		assetService.save(asset);
-		assetService.save(leg);
+		if (!leg.isRealized()) {
+			Asset asset = leg.getAmortisation().getAsset();
+			Double currentValue = asset.getCurrentValue();
+			currentValue = currentValue - leg.getAmount(); // Amount of the depreciation.
+			asset.setCurrentValue(currentValue);
+			leg.setRealized(true);
+			assetService.save(asset);
+			assetService.save(leg);
+		}
+		else 
+		{
+			Logger.getGlobal().log(Level.FINE,"allready realized");
+
+		}
+		return "";
 	}
 
 }
