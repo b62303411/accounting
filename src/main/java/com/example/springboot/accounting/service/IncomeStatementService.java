1bbb930878eb946bd8592c15edca1d7427f85f73
b62303411 <samuel.ars@gmail.com>
2023-08-12 16:55:07 -0400

    remove from tax

--- src/main/java/com/example/springboot/accounting/service/IncomeStatementService.java
+++ src/main/java/com/example/springboot/accounting/service/IncomeStatementService.java
@@ -390,15 +390,29 @@ public class IncomeStatementService {
 		default:
 			total = transaction.getAmount();
 		}
-		double TPS_RATE = SalesTaxeRates.TPS_RATE;
-		double TVQ_RATE = SalesTaxeRates.TVQ_RATE;
-		double beforeTaxes = total / (1 + TPS_RATE + TPS_RATE * TVQ_RATE + TVQ_RATE);
-		// Calculate TPS and TVQ
-		double tps = beforeTaxes * TPS_RATE;
-		double tvq = (beforeTaxes + tps) * TVQ_RATE;
-		ex.setTps(tps);
-		ex.setTvq(tvq);
-		ex.setTotalBeforeSalesTaxes(beforeTaxes);
+		switch(ex.getExpenseType()) 
+		{
+		case Interets:
+		case FraisBancaires:
+		case Amortisation:
+		case Immobilisation:
+		case Taxes:
+			ex.setTotalBeforeSalesTaxes(total);
+			break;
+			
+		default:
+			double TPS_RATE = SalesTaxeRates.TPS_RATE;
+			double TVQ_RATE = SalesTaxeRates.TVQ_RATE;
+			double beforeTaxes = total / (1 + TPS_RATE + TPS_RATE * TVQ_RATE + TVQ_RATE);
+			// Calculate TPS and TVQ
+			double tps = beforeTaxes * TPS_RATE;
+			double tvq = (beforeTaxes + tps) * TVQ_RATE;
+			ex.setTps(tps);
+			ex.setTvq(tvq);
+			ex.setTotalBeforeSalesTaxes(beforeTaxes);
+		
+		}
+
 		// ex.setExpenseType(transaction.getType());
 		ex.setDescription(transaction.getDescription());
 		ex.setDate(transaction.getDate());
