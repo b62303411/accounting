6a3f6663f1024503431a8c4b34f0d5394036b113
b62303411 <samuel.ars@gmail.com>
2025-06-09 14:57:08 -0400

    fix issues

--- src/main/java/com/sam/accounting/configuration/DefaultUser.java
+++ src/main/java/com/sam/accounting/configuration/DefaultUser.java
@@ -1,10 +1,20 @@
 package com.sam.accounting.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DefaultUser {
 	@Value("${default_user}")
 	private String default_user;
 	@Value("${default_password}")
 	private String default_password;
+	public String getDefault_user() {
+		return default_user;
+	}
+	public String getDefault_password() {
+		return default_password;
+	}
+	
+	
 }
--- src/main/java/com/sam/accounting/model/UserInitializer.java
+++ src/main/java/com/sam/accounting/model/UserInitializer.java
@@ -1,8 +1,10 @@
 package com.sam.accounting.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.sam.accounting.configuration.DefaultUser;
 import com.sam.accounting.model.entities.AppUser;
 import com.sam.accounting.repository.AppUserRepository;
 
@@ -15,7 +17,8 @@ public class UserInitializer {
 
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder passwordEncoder;
-
+    @Autowired
+    private DefaultUser defaultUser;
     public UserInitializer(AppUserRepository appUserRepository) {
         this.appUserRepository = appUserRepository;
         this.passwordEncoder = new BCryptPasswordEncoder(); // Use @Bean in config if needed
@@ -23,10 +26,10 @@ public class UserInitializer {
 
     @PostConstruct
     public void init() {
-        if (appUserRepository.findByUsername("samuel").isEmpty()) {
+        if (appUserRepository.findByUsername(defaultUser.getDefault_user()).isEmpty()) {
             AppUser user = new AppUser();
-            user.setUsername("samuel");
-            user.setPassword(passwordEncoder.encode("25528B24isk30!!")); // hash it
+            user.setUsername(defaultUser.getDefault_user());
+            user.setPassword(passwordEncoder.encode(defaultUser.getDefault_password())); // hash it
             user.setEnabled(true);
             appUserRepository.save(user);
         }
